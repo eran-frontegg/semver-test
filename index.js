4e673fc195b669f00746a32f99d0d23c8c1a1ac3
Eran Kricheli <eran.kricheli@frontegg.com>
2022-10-30 11:13:20 +0200

    feat(index): modify message

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 module.exports.newHello = (msg) => { console.log(`Message: ${msg}`); };
-module.exports.hello = (msg) => { console.log('original') };
+module.exports.hello = (msg) => { console.log('This is the original') };
